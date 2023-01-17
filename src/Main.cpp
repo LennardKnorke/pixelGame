a60ee3e2baed10f13ce630acd5e0df43f13bedac
LennardKnorke <janlennardgrabowski@gmx.de>
2023-01-17 15:29:15 +0100

    some fixes. no clicks working yet

--- src/Main.cpp
+++ src/Main.cpp
@@ -42,18 +42,21 @@ class Layer {
     int nDefaultButtons;
     int nNonDefaultButtons;
     int nButtons;
-    Image *imageReferences;
-    Texture2D *layerLoadedTextures;
+    Image *imagesClicked;
+    Image *imagesUnclicked;
+    Texture2D *layerTexturesUnclicked;
+    Texture2D *layerTexturesClicked;
     int *positions_X;
     int *positions_Y;
-    void initiate (int lay, int nProfiles, int nWorlds, Image UnprocessedImages[], int fontSize){
+
+    Sound clickS;
+    void initiate (int lay, nlohmann::json nProfiles, Image UnprocessedImages[], int fontSize){
         LayerNr = lay;
         const char *textToDraw[7];
-        int buttonindexing = 0;
         //STARTING/ PICK PROFILE
         if (LayerNr == 0){
             nDefaultButtons = 1;
-            nNonDefaultButtons = nProfiles;
+            nNonDefaultButtons = nProfiles["nprofiles"];
             textToDraw[0] = "NEW PROFILE";
         //MAIN SCREEN
         } else if (LayerNr == 1){
@@ -75,7 +78,7 @@ class Layer {
         //HOSTING. CREATE OR PICK WORLD
         } else if (LayerNr == 4){
             nDefaultButtons = 1;
-            nNonDefaultButtons = nWorlds;
+            nNonDefaultButtons = 0;
             textToDraw[0] = "NEW WORLD";
         //JOINING. ENTER IP AND PORT
         } else if (LayerNr == 5){
@@ -88,47 +91,58 @@ class Layer {
         
         positions_X = (int *)malloc(sizeof(int) * nButtons);
         positions_Y = (int *)malloc(sizeof(int) * nButtons);
-        imageReferences = (Image *)malloc(sizeof(Image) * (nButtons * 2));//One clicked and an unclicked version
-        layerLoadedTextures = (Texture2D *)malloc(sizeof(Image) * (nButtons * 2));
+        imagesClicked = (Image *)malloc(sizeof(Image) * (nButtons));//One clicked and an unclicked version
+        imagesUnclicked = (Image *)malloc(sizeof(Image) * (nButtons));//One clicked and an unclicked version
+
         
-        buttonindexing = 0;
+        layerTexturesClicked = (Texture2D *)malloc(sizeof(Image) * (nButtons));
+        layerTexturesUnclicked = (Texture2D *)malloc(sizeof(Image) * (nButtons));
+
+        //in the unprocessed. the button images have index 2 and 3. and the world buttons have index 4 and 5.
+
+        //first iterate through default buttons and create them
         for (int i = 0 ; i < nDefaultButtons; i ++){
-            imageReferences[buttonindexing] = UnprocessedImages[2];
-            imageReferences[buttonindexing + 1] = UnprocessedImages[3];
-            ImageDrawText(&imageReferences[buttonindexing], textToDraw[i], ((imageReferences[buttonindexing].width / 2) - (MeasureText("PLAY", fontSize) / 2)), ((imageReferences[buttonindexing].height / 2) - (fontSize / 2)), fontSize, BLACK);
-            ImageDrawText(&imageReferences[buttonindexing + 1], textToDraw[i], ((imageReferences[buttonindexing + 1].width / 2) - (MeasureText("PLAY", fontSize) / 2)), ((imageReferences[buttonindexing].height / 2) - (fontSize / 2)), fontSize, BLACK);
-            positions_X[i] = ((screenWidth / 2) - (imageReferences[buttonindexing].width / 2));
+            imagesUnclicked[i] = UnprocessedImages[3];
+            imagesClicked[i] = UnprocessedImages[2];
+            ImageDrawText(&imagesUnclicked[i], textToDraw[i], ((imagesUnclicked[i].width / 2)), ((imagesUnclicked[i].height / 2)), fontSize, BLACK);
+            ImageDrawText(&imagesClicked[i], textToDraw[i], ((imagesClicked[i].width / 2)), ((imagesClicked[i].height / 2)), fontSize, BLACK);
+            positions_X[i] = ((screenWidth / 2) - (imagesUnclicked[i].width / 2));
             positions_Y[i] = ((screenHeight / (nButtons+1)) * (i + 1));
-            layerLoadedTextures[buttonindexing] = LoadTextureFromImage(imageReferences[buttonindexing]);
-            layerLoadedTextures[buttonindexing + 1] = LoadTextureFromImage(imageReferences[buttonindexing]);
-
-
-            buttonindexing += 2;
-        }
-        for (int i = 0; i < nNonDefaultButtons; i++){
+            layerTexturesClicked[i] = LoadTextureFromImage(imagesUnclicked[i]);
+            layerTexturesUnclicked[i] = LoadTextureFromImage(imagesClicked[i]);
 
         }
+        if (LayerNr == 0){
+            for (int i = 0; i < nNonDefaultButtons; i++){
+
+            }
+        }
+        
 
     }
     void drawThisShit(int mX, int mY){
-        for (int i = 0; i < nButtons; i += 2){
-            if ((positions_X[i] < mX) && ((positions_X[i] + imageReferences[i].width) > mX)){
-                DrawTexture(layerLoadedTextures[i + 1], positions_X[i], positions_Y[i], WHITE);
+        for (int i = 0; i < nButtons; i++){
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && ((mX > ((screenWidth / 2) - (imagesUnclicked[i].width / 2))) && mX < ((screenWidth / 2) + (imagesUnclicked[i].width / 2))) && ((mY > ((screenHeight / (nButtons+1)) * (i + 1))) && (mY < ((screenHeight / (nButtons+1)) * (i + 1)) + imagesClicked[i].height))){
+                DrawTexture(layerTexturesClicked[i], positions_X[i], positions_Y[i], WHITE);
             } else {
-                DrawTexture(layerLoadedTextures[i], positions_X[i + 1], positions_Y[i + 1], WHITE);
+                DrawTexture(layerTexturesUnclicked[i], positions_X[i], positions_Y[i], WHITE);
             }
         }
     }
 
     void destroy(void){
-        for (int i = 0; i < nButtons * 2; i++){
-            UnloadTexture(layerLoadedTextures[i]);
-            UnloadImage(imageReferences[i]);
+        for (int i = 0; i < nButtons; i++){
+            UnloadTexture(layerTexturesClicked[i]);
+            UnloadImage(imagesClicked[i]);
+            UnloadTexture(layerTexturesUnclicked[i]);
+            UnloadImage(imagesUnclicked[i]);
         }
         free(positions_X);
         free(positions_Y);
-        free(imageReferences);
-        free(layerLoadedTextures);
+        free(imagesUnclicked);
+        free(layerTexturesUnclicked);
+        free(imagesClicked);
+        free(layerTexturesClicked);
     }
 
 };
@@ -150,9 +164,6 @@ nlohmann::json read_json_file (const std::string &path){
 int run_Menu(Image imageRefList[NUMBER_OF_DIFFERENT_MENU_TEXTURES], nlohmann::json configFile);
 int play_Game(void);
 int load_World(void);
-void resizeMenuTextures (Image imgRefList[NUMBER_OF_DIFFERENT_MENU_TEXTURES], int newx, int newy, Texture2D loadedTextures[NUMBER_OF_DIFFERENT_MENU_TEXTURES]);
-
-
 
 //Main script. loads default settings, creates configs and loads menu texture before going into the game cycle
 //Within the game cycle one may switch between 1. the menu, 2. loading (connecting to server etc..) and 3.The actual game.
@@ -161,8 +172,9 @@ int main (int argc, char *args[]){
     //Load settings, screen infos and stuff
     nlohmann::json configs = read_json_file("../config.json");
     
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     bool fullScreen;
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitAudioDevice();
     InitWindow(screenWidth, screenHeight, "Fuck yeah I'm a god");
     //If its the first time starting the game, look for default options and save them in the json
     if (configs["first"] == true){
@@ -235,7 +247,7 @@ int main (int argc, char *args[]){
     Image preprocessedenemySprites[NUMBER_ENEMY_TEXTURES];
 */
     
-    HideCursor();
+    //HideCursor();
     
     /*Prepare here:  two sockets which either through threading or sync stuff are being prepared during loading phase
         One client socket and one server socket.
@@ -270,29 +282,23 @@ int main (int argc, char *args[]){
 }
 
 
-void resizeMenuTextures (Image imgRefList[NUMBER_OF_DIFFERENT_MENU_TEXTURES], int newx, int newy, Texture2D loadedTextures[NUMBER_OF_DIFFERENT_MENU_TEXTURES]){
-    for (int i = 0; i < NUMBER_OF_DIFFERENT_MENU_TEXTURES; i++){
-        ImageResize(&imgRefList[i], newx, newy);
-        UnloadTexture(loadedTextures[i]);
-        loadedTextures[i] = LoadTextureFromImage(imgRefList[i]);
-    }
-}
+
 
 int run_Menu(Image imageRefList[NUMBER_OF_DIFFERENT_MENU_TEXTURES], nlohmann::json configFile){
     //Info used to create default buttons
-    int fontGroesse = 35 * screenRatios[1];
-    int nProfiles = configFile["nprofiles"];
+    int fontGroesse = 20 * screenRatios[1];
     //Now the default buttons are created. png suited for buttons are given, their displayed text, loaded for each of their two states [clicked or not clicked] and at last moved into gpu
     
     //0-5 layers... 0: pick profile. 1:play, options, exit. 2:options content. 3:Host or Join. 4:hosting, create or pick world. 5:joining enter host ip and port
-
+    
+    Sound clickSound = LoadSound("../sound/mclick.wav");
     Layer Layers[6];
     
     for (int k = 0; k < 6; k++){
-        Layers[k].initiate(k, nProfiles, 0, imageRefList, fontGroesse);
+        Layers[k].initiate(k, configFile, imageRefList, fontGroesse);
     }
 
-
+    
 
     //add cursor and background
     Texture2D CurserSprite = LoadTextureFromImage(imageRefList[1]);
@@ -301,7 +307,7 @@ int run_Menu(Image imageRefList[NUMBER_OF_DIFFERENT_MENU_TEXTURES], nlohmann::js
     int mouseX = GetMouseX();
     int mouseY = GetMouseY();
     int menu_running = 1;
-    int Current_Layer = 1;
+    int Current_Layer = 0;
     while (menu_running == 1) {
         mouseX = GetMouseX();
         mouseY = GetMouseY();
@@ -311,7 +317,7 @@ int run_Menu(Image imageRefList[NUMBER_OF_DIFFERENT_MENU_TEXTURES], nlohmann::js
             //Draw default buttons available for the current layer
             Layers[Current_Layer].drawThisShit(mouseX, mouseY);
             DrawFPS(0,0);
-            DrawTexture(CurserSprite, mouseX + (CurserSprite.width / 2), mouseY + (CurserSprite.height / 2), WHITE);
+            DrawTexture(CurserSprite, mouseX - (CurserSprite.width / 2), mouseY - (CurserSprite.height / 2), WHITE);
         EndDrawing();
 
         if (IsKeyPressed(KEY_ESCAPE) == true){
@@ -321,10 +327,19 @@ int run_Menu(Image imageRefList[NUMBER_OF_DIFFERENT_MENU_TEXTURES], nlohmann::js
                 Current_Layer -= 1;
             }
         }
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+            PlaySound(clickSound);
+        }
+        
+        //Who thought, not making this a configuration set at the beginning, is a good idea...
+        if (IsWindowFocused() == false){
+            MinimizeWindow();
+        } 
         
     }
 
     //Unload Texture and Images. Cause why do we need them...
+    UnloadSound(clickSound);
     UnloadTexture(BackGroundSprite);
     UnloadTexture(CurserSprite);
     for (int k = 0; k < 6; k++){
